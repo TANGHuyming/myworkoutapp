2039e93dd77cfa6a14e803eb6b20c45182e07ac2
TANGHuyming <2023277tang@aupp.edu.kh>
2025-12-08 09:47:10 +0700

    added brzycki estimation component

--- app/components/MaxRepChart.tsx
+++ app/components/MaxRepChart.tsx
@@ -1,7 +1,9 @@
 "use client";
 import Chart from "chart.js/auto";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { GiWeightLiftingUp } from "react-icons/gi";
+import { inputStyling, buttonStyling } from "./AddSet";
+import { pixelifySans } from "../layout";
 
 interface dataType {
     date: Date,
@@ -10,6 +12,9 @@ interface dataType {
 };
 
 export default function MaxRepChart() {
+    const [weight, setWeight] = useState(0);
+    const [reps, setReps] = useState(0);
+    const [estimation, setEstimation] = useState(0);
     const data = [
         {date: new Date("2025-12-06T12:41:00Z"), exercise: "Squat", maxWeight: 100},
         {date: new Date("2026-12-06T12:41:00Z"), exercise: "Squat", maxWeight: 110},
@@ -17,7 +22,6 @@ export default function MaxRepChart() {
         {date: new Date("2026-12-07T12:41:00Z"), exercise: "Deadlift", maxWeight: 200},
         {date: new Date("2025-11-08T12:41:00Z"), exercise: "Benchpress", maxWeight: 40},
         {date: new Date("2026-11-08T12:41:00Z"), exercise: "Benchpress", maxWeight: 100},
-
     ]
 
     const createChart = (canvas: any, type: any, data: dataType[], filter: String, backgroundColor: String, borderColor: String) => {
@@ -44,6 +48,18 @@ export default function MaxRepChart() {
         )
     }
 
+    const handleSubmit = (e: any) => {
+        e.preventDefault();
+        const formData = new FormData(e.target)
+        const data = Object.fromEntries(formData.entries());
+        console.log(data);
+
+        const brzycki = weight / (1.0278 - 0.0278 * reps);
+        setEstimation(parseFloat(brzycki.toFixed(2)));
+        setWeight(0);
+        setReps(0);
+    }
+
     useEffect(() => {
         const squatOneRmChart : HTMLCanvasElement = document.getElementById("squatOneRmChart") as HTMLCanvasElement;
         const deadliftOneRmChart : HTMLCanvasElement = document.getElementById("deadliftOneRmChart") as HTMLCanvasElement;
@@ -58,6 +74,11 @@ export default function MaxRepChart() {
         charts.map((chart) => createChart(chart.canvas, chart.line, chart.data, chart.filter, chart.bg, chart.bd))
 
     }, []);
+
+    useEffect(() => {
+        const result = document.getElementById("result") as HTMLHeadingElement;
+        result.textContent = `${String(estimation)} Kg`;
+    }, [estimation])
 
     return(
         <div className="bg-gray-300 p-10 grid grid-cols-2 justify-center items-center gap-2 select-none">
@@ -79,6 +100,36 @@ export default function MaxRepChart() {
             <div>
                 <h2 className="wave text-xl font-semibold mb-5 flex flex-row justify-center items-center">Barbell Benchpress 1RM</h2>
                 <canvas id="benchOneRmChart" className="bg-gray-200 p-2 rounded-lg shadow-lg shadow-gray-400"></canvas>
+            </div>
+
+            <div className="col-span-2 mt-10 flex flex-col justify-center items-center">
+                <h2 className="text-3xl font-bold mb-5 flex flex-row justify-center items-center gap-5">
+                    Estimate Your 1RM
+                    <GiWeightLiftingUp className="text-2xl expand"/>
+                </h2>
+
+                <div className="grid grid-cols-2 grid-rows-2 justify-center items-center gap-5">
+                    <dl className="col-start-1 row-start-1">
+                        <dt className="font-bold italic">Brzycki Formula:</dt>
+                        <dd className="ml-5">The formula used to estimate your maximum lifting weight using a lighter load that you can lift for multiple reps.</dd>
+                        <dd className="m-5 bg-gray-200 text-center p-2 rounded-md shadow-lg shadow-gray-400">Estimation = Weight / (1.0278 - 0.0278 * Reps)</dd>
+                    </dl>
+
+                    <div className="col-start-1 row-start-2 h-full flex flex-col justify-start items-start gap-2">
+                        <h2 className="text-2xl font-semibold">Your Estimated Heaviest Lift is: </h2>
+                        <h2 id="result" className={`text-5xl font-bold ${pixelifySans.className}`}>{estimation} Kg</h2>
+                    </div>
+
+                    <form onSubmit={(e) => handleSubmit(e)} className="h-full col-start-2 row-span-2 grid grid-cols-2 p-5 gap-2 bg-gray-200 rounded-lg shadow-lg shadow-gray-400 justify-center items-center">
+                        <label htmlFor="weight">Enter the weight (kg) that you can lift: </label>
+                        <input className={inputStyling} type="number" id="weight" name="weight" required value={weight} onChange={(e) => setWeight(Number(e.target.value))}/>
+                        <label htmlFor="reps">Enter the number reps that you lifted (Given {weight}kg):</label>
+                        <input className={inputStyling} type="number" id="reps" name="reps" required value={reps} onChange={(e) => setReps(Number(e.target.value))}/>
+                    
+                        <button className={`col-span-2 ${buttonStyling}`} type="submit">Submit</button>
+                    </form>
+                    
+                </div>
             </div>
         </div>
     );
